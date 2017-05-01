210f78007395d3ce98a0c2c684818f0cc5ea3a46
alec aivazis <alec@aivazis.com>
2017-04-30 22:31:51 -0700

    logging service stores logs by similar name in memory

--- logging/logging.go
+++ logging/logging.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/nautilus/events"
 
+	"encoding/json"
+
 	"github.com/AlecAivazis/maestro/common"
 )
 
@@ -13,11 +15,33 @@ type MaestroLogging struct {
 	events.EventBroker
 }
 
+var logCache = make(map[string][]string)
+
 func (s *MaestroLogging) HandleAction(a *events.Action) {
 	// what we do with the action depends on the type
 	switch a.Type {
 	// if we are retrieving the information for a given repo
 	case common.ActionLogAction:
-		fmt.Println(a.Payload)
+		// a log payload to write to
+		payload := common.LogPayload{}
+
+		// try to parse the payload into the struct we know it to follow
+		err := json.Unmarshal([]byte(a.Payload), &payload)
+		// if something went wrong
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		// look for logs with the current label
+		val, ok := logCache[payload.Label]
+		// if there is an entry in the cache
+		if ok {
+			// add the payload to the cache
+			logCache[payload.Label] = append(val, payload.Payload)
+		} else {
+			// otherwise there is no log with that label so save one
+			logCache[payload.Label] = []string{payload.Payload}
+		}
 	}
 }
